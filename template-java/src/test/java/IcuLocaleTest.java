1864ae8177558b95d6fa480792b40f2041fe4f1f
Min Huang <min.huang@alumni.usc.edu>
2024-04-02 16:21:09 -0700

    adds tests from other java repos

---
+++ template-java/src/test/java/IcuLocaleTest.java
@@ -0,0 +1,246 @@
+package com.retiman.java;
+
+import java.util.Locale;
+
+import com.ibm.icu.util.IllformedLocaleException;
+import com.ibm.icu.util.ULocale;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+
+public final class IcuLocaleTest {
+  @Test
+  public void testStringRepresentations() {
+    ULocale locale = new ULocale.Builder()
+        .setLanguage("zh")
+        .setScript("Hans")
+        .setRegion("CN")
+        .setVariant("wadegile")
+        .setExtension('t', "en")
+        .setExtension('u', "latn")
+        .setExtension('x', "apex")
+        .build();
+
+    // The ICU locale toString method is designed for debuggability and not intended to be any type of standard.  It's
+    // not possible to go from this string representation to a locale or a language tag.  Notably it is also different
+    // from the JDK locale's toString representation.
+    //
+    // The script added after the language code to more closely mirror the IETF language tag.
+    assertThat(locale.toString()).isEqualTo("zh_Hans_CN_WADEGILE@attribute=latn;t=en;x=apex");
+
+    // An interesting thing to note is that ICU locales will uppercase the variant; however, it gets the display variant
+    // correct.  It seems that the JDK version of the ICU library will uppercase no matter what.  While the script and
+    // region subtags must be title case and uppercase, respectively, no requirement is given to variant subtags.
+    //
+    // That said, the recommendation is that variant subtags are lowercase.  This is specified in the ICU documentation
+    // about case normalization.  Not sure why, but ok.
+    assertThat(locale.getVariant()).isEqualTo("WADEGILE");
+    assertThat(locale.getDisplayVariant()).isEqualTo("Wade-Giles Romanization");
+
+    // The IETF language tag; however, can be used to reconstruct a locale; it will agree with the JDK's representation
+    // of the language tag.  In spite of the case normalization applied to variants, you will get the correct IETF
+    // language tag (with a lowercased variant) if you ask for it.
+    String tag = "zh-Hans-CN-wadegile-t-en-u-latn-x-apex";
+    assertThat(locale.toLanguageTag()).isEqualTo(tag);
+    assertThat(ULocale.forLanguageTag(tag)).isEqualTo(locale);
+
+    // Unfortunately, though, if you try to convert this corresponding ULocale to a JDK locale, the variant will remain
+    // uppercased.
+    Locale jdkLocale = Locale.forLanguageTag(tag);
+    ULocale icuLocale = ULocale.forLanguageTag(tag);
+    assertThat(jdkLocale).isNotEqualTo(icuLocale);
+
+    // To construct a corresponding JDK locale, the variant must be lowercased.
+    Locale transformed = new Locale.Builder()
+        .setLocale(icuLocale.toLocale())
+        .setVariant(icuLocale.getVariant().toLowerCase(Locale.US))
+        .build();
+    assertThat(jdkLocale).isEqualTo(transformed);
+  }
+
+  @Test
+  public void testNonEquivalentLegacyCodes() {
+    ULocale iw = new ULocale("iw");
+    ULocale he = new ULocale("he");
+
+    // The ICU locale class is mostly WYSIWYG.  If you asked for "iw", you get "iw".  If you asked for "he", you'll get
+    // "he".
+    assertThat(iw.toString()).isEqualTo("iw");
+    assertThat(he.toString()).isEqualTo("he");
+    assertThat(iw.getLanguage()).isEqualTo("iw");
+    assertThat(he.getLanguage()).isEqualTo("he");
+
+    // The only difference is that the IETF language tags must be valid, so you'll get the same behavior as the JDK
+    // locale.
+    assertThat(iw.toLanguageTag()).isEqualTo("he");
+    assertThat(he.toLanguageTag()).isEqualTo("he");
+
+    // In fact, the two locales are not even considered equivalent.
+    assertThat(iw).isNotEqualTo(he);
+
+    // Their language tags are though.
+    assertThat(iw.toLanguageTag()).isEqualTo(he.toLanguageTag());
+  }
+
+  @Test
+  public void testLanguageCodes() {
+    // A list of ISO-639 codes can be found here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    ULocale afrikaans2c = new ULocale("af");
+    ULocale afrikaans3c = new ULocale("afr");
+
+    assertThat(afrikaans2c.toString()).isEqualTo("af");
+    assertThat(afrikaans2c.toLanguageTag()).isEqualTo("af");
+
+    // In contrast to the JDK locale class, proper normalizations and canonicalizations will be applied.  For "afr",
+    // there is no ambiguity between the "af" code and "afr", so "af" is the normalziation.
+    assertThat(afrikaans3c.toString()).isEqualTo("af");
+    assertThat(afrikaans3c.getLanguage()).isEqualTo("af");
+    assertThat(afrikaans3c.toLanguageTag()).isEqualTo("af");
+
+    // They are also considered the same locale.
+    assertThat(afrikaans2c).isEqualTo(afrikaans3c);
+  }
+
+  @Test
+  public void testLanguageCodesNormalized() {
+    ULocale lower = new ULocale("en");
+    ULocale upper = new ULocale("EN");
+
+    assertThat(lower.getLanguage()).isEqualTo("en");
+    assertThat(upper.getLanguage()).isEqualTo("en");
+    assertThat(lower.toLanguageTag()).isEqualTo("en");
+    assertThat(upper.toLanguageTag()).isEqualTo("en");
+    assertThat(lower).isEqualTo(upper);
+  }
+
+  @Test
+  public void testInvalidLanguageCodes() {
+    // A list of ISO-639 codes can be found here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    ULocale locale = new ULocale("abcd");
+
+    assertThat(locale.toLanguageTag()).isEqualTo("abcd");
+  }
+
+  @Test
+  public void testInvalidTooShortScript() {
+    Throwable thrown = catchThrowable(() -> new ULocale.Builder()
+        .setLanguage("zh")
+        .setScript("Han")
+        .build());
+
+    assertThat(thrown).isExactlyInstanceOf(IllformedLocaleException.class);
+  }
+
+  @Test
+  public void testInvalidScriptForLanguage() {
+    ULocale locale = new ULocale.Builder()
+        .setLanguage("en")
+        .setScript("Hant")
+        .build();
+
+    assertThat(locale.toString()).isEqualTo("en_Hant");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-Hant");
+  }
+
+  @Test
+  public void testRegionCodes() {
+    // A list of ISO-3661 codes can be found here: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+    ULocale franceFr = new ULocale("fr", "FR");
+    ULocale france250 = new ULocale("fr", "250");
+
+    assertThat(franceFr.toLanguageTag()).isEqualTo("fr-FR");
+    assertThat(france250.toLanguageTag()).isEqualTo("fr-250");
+    assertThat(franceFr).isNotEqualTo(france250);
+  }
+
+  @Test
+  public void testRegionCodesNormalized() {
+    ULocale lower = new ULocale("fr", "fr");
+    ULocale upper = new ULocale("fr", "FR");
+
+    assertThat(lower.getCountry()).isEqualTo("FR");
+    assertThat(upper.getCountry()).isEqualTo("FR");
+    assertThat(lower.toLanguageTag()).isEqualTo("fr-FR");
+    assertThat(upper.toLanguageTag()).isEqualTo("fr-FR");
+    assertThat(lower).isEqualTo(upper);
+  }
+
+  @Test
+  public void testVariantsAreUppercasedExceptInLanguageTags() {
+    ULocale locale = new ULocale("en", "US", "variant");
+
+    assertThat(locale.getVariant()).isEqualTo("VARIANT");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-US-variant");
+  }
+
+  @Test
+  public void testInvalidShortVariantsAreConvertedToExtensions() {
+    ULocale locale = new ULocale("en", "US", "1");
+
+    // The toString value gives the variant value as is.  Since the toString value is not part of any specification,
+    // the variant part can be as many characters as you want.
+    assertThat(locale.toString()).isEqualTo("en_US_1");
+
+    // Variants must be between 5-8 characters, if they are too short, they are converted to extensions.
+    assertThat(locale.getVariant()).isEqualTo("1");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-US-x-lvariant-1");
+
+    // They can be converted back to ULocales as well.
+    ULocale converted = ULocale.forLanguageTag("en-US-x-lvariant-1");
+
+    assertThat(converted.getVariant()).isEqualTo("1");
+
+    ULocale extension = new ULocale.Builder()
+        .setLanguage("en")
+        .setRegion("US")
+        .setExtension('x', "lvariant-1")
+        .build();
+    assertThat(extension).isEqualTo(converted);
+  }
+
+  @Test
+  public void testInvalidLongVariantsAreDiscarded() {
+    ULocale locale = new ULocale("en", "US", "thisvariantistoolong");
+
+    // The toString value gives the variant value as is.  Since the toString value is not part of any specification,
+    // the variant part can be as many characters as you want.
+    assertThat(locale.toString()).isEqualTo("en_US_THISVARIANTISTOOLONG");
+
+    // Variants must be between 5-8 characters, if they are too long, they are dropped.  This is because extensions must
+    // be between 2-8 characters, and separated by hyphens.  Converting to an extension would result in a too long
+    // extension, and instead the variant is dropped when creating a language tag.
+    assertThat(locale.getVariant()).isEqualTo("THISVARIANTISTOOLONG");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-US");
+  }
+
+  @Test
+  public void testInvalidExtensionsThatAreTooShort() {
+    ULocale locale = new ULocale.Builder()
+        .setLanguage("en")
+        .setExtension('x', "1")
+        .build();
+
+    assertThat(locale.toString()).isEqualTo("en@x=1");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-x-1");
+
+    locale = new ULocale.Builder()
+        .setLanguage("en")
+        .setExtension('x', "")
+        .build();
+
+    assertThat(locale.toString()).isEqualTo("en");
+    assertThat(locale.toLanguageTag()).isEqualTo("en");
+  }
+
+  @Test
+  public void testInvalidExtensionsThatAreTooLongAreIllformed() {
+    Throwable thrown = catchThrowable(() -> new ULocale.Builder()
+        .setLanguage("en")
+        .setExtension('x', "thisextensionistoolong")
+        .build());
+
+    assertThat(thrown).isExactlyInstanceOf(IllformedLocaleException.class);
+  }
+}
---
+++ template-java/src/test/java/JdkLocaleTest.java
@@ -0,0 +1,226 @@
+package com.retiman.java;
+
+import java.util.IllformedLocaleException;
+import java.util.Locale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+
+public final class JdkLocaleTest {
+  @Test
+  public void testStringRepresentations() {
+    Locale locale = new Locale.Builder()
+        .setLanguage("zh")
+        .setScript("Hans")
+        .setRegion("CN")
+        .setVariant("wadegile")
+        .setExtension('t', "en")
+        .setExtension('u', "latn")
+        .setExtension('x', "apex")
+        .build();
+
+    // JDK locale toString method is designed for debuggability and not intended to be any type of standard.  It's not
+    // possible to go from this string representation to a locale or a language tag.
+    //
+    // The JDK documentation states that "clients who parse the output of toString into language, country, and variant
+    // fields can continue to do so (although this is strongly discouraged), although the variant field will have
+    // additional information in it if script or extensions are present."
+    //
+    // Parsing the toString output of locales with scripts is not advised.
+    //
+    // See https://docs.oracle.com/javase/7/docs/api/java/util/Locale.html
+    assertThat(locale.toString()).isEqualTo("zh_CN_wadegile_#Hans_t-en-u-latn-x-apex");
+
+    // The JDK locale class will not have information about display variants (in this case).
+    assertThat(locale.getVariant()).isEqualTo("wadegile");
+    assertThat(locale.getDisplayVariant()).isEqualTo("wadegile");
+
+    // The IETF language tag; however, can be used to reconstruct a locale.
+    String tag = "zh-Hans-CN-wadegile-t-en-u-latn-x-apex";
+    assertThat(locale.toLanguageTag()).isEqualTo(tag);
+    assertThat(Locale.forLanguageTag(tag)).isEqualTo(locale);
+  }
+
+  @Test
+  public void testEquivalentLegacyCodes() {
+    Locale iw = new Locale("iw");
+    Locale he = new Locale("he");
+
+    // In older versions of Java, the JDK locale class conveniently converts the ISO-639 code for "he" (Hebrew) to "iw"
+    // for you.  The "iw" code was changed in 1989 to "he", so it is considered the legacy code, but the JDK was big on
+    // backwards compatibility for this matter.  This is no longer true in OpenJDK 17.
+    //
+    // The toString method and getLanguage method will always give you back the OLD value, no matter what.
+    assertThat(iw.toString()).isEqualTo("he");
+    assertThat(he.toString()).isEqualTo("he");
+    assertThat(iw.getLanguage()).isEqualTo("he");
+    assertThat(he.getLanguage()).isEqualTo("he");
+
+    // However, the "iw" language tag isn't valid, so you'll always get the "he" tag back.
+    assertThat(iw.toLanguageTag()).isEqualTo("he");
+    assertThat(he.toLanguageTag()).isEqualTo("he");
+
+    // The two locales are considered equivalent though.
+    assertThat(iw).isEqualTo(he);
+
+    // The JDK docs recommend that you do this for comparisons:
+    assertThat(iw.getLanguage()).isEqualTo(he.getLanguage());
+  }
+
+  @Test
+  public void testLanguageCodes() {
+    // A list of ISO-639 codes can be found here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    Locale afrikaans2c = new Locale("af");
+    Locale afrikaans3c = new Locale("afr");
+
+    assertThat(afrikaans2c.toString()).isEqualTo("af");
+    assertThat(afrikaans2c.toLanguageTag()).isEqualTo("af");
+    assertThat(afrikaans3c.toString()).isEqualTo("afr");
+    assertThat(afrikaans3c.toLanguageTag()).isEqualTo("afr");
+    assertThat(afrikaans2c).isNotEqualTo(afrikaans3c);
+  }
+
+  @Test
+  public void testLanguageCodesNormalized() {
+    Locale lower = new Locale("en");
+    Locale upper = new Locale("EN");
+
+    assertThat(lower.getLanguage()).isEqualTo("en");
+    assertThat(upper.getLanguage()).isEqualTo("en");
+    assertThat(lower.toLanguageTag()).isEqualTo("en");
+    assertThat(upper.toLanguageTag()).isEqualTo("en");
+    assertThat(lower).isEqualTo(upper);
+  }
+
+  @Test
+  public void testInvalidLanguageCodes() {
+    // A list of ISO-639 codes can be found here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    Locale locale = new Locale("abcd");
+
+    assertThat(locale.toLanguageTag()).isEqualTo("abcd");
+  }
+
+  @Test
+  public void testInvalidTooShortScript() {
+    Throwable thrown = catchThrowable(() -> new Locale.Builder()
+        .setLanguage("zh")
+        .setScript("Han")
+        .build());
+
+    assertThat(thrown).isExactlyInstanceOf(IllformedLocaleException.class);
+  }
+
+  @Test
+  public void testInvalidScriptForLanguage() {
+    Locale locale = new Locale.Builder()
+        .setLanguage("en")
+        .setScript("Hant")
+        .build();
+
+    assertThat(locale.toString()).isEqualTo("en__#Hant");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-Hant");
+  }
+
+  @Test
+  public void testRegionCodes() {
+    // A list of ISO-3661 codes can be found here: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+    Locale franceFr = new Locale("fr", "FR");
+    Locale france250 = new Locale("fr", "250");
+
+    assertThat(franceFr.toLanguageTag()).isEqualTo("fr-FR");
+    assertThat(france250.toLanguageTag()).isEqualTo("fr-250");
+    assertThat(franceFr).isNotEqualTo(france250);
+  }
+
+  @Test
+  public void testRegionCodesNormalized() {
+    Locale lower = new Locale("fr", "fr");
+    Locale upper = new Locale("fr", "FR");
+
+    assertThat(lower.getCountry()).isEqualTo("FR");
+    assertThat(upper.getCountry()).isEqualTo("FR");
+    assertThat(lower.toLanguageTag()).isEqualTo("fr-FR");
+    assertThat(upper.toLanguageTag()).isEqualTo("fr-FR");
+    assertThat(lower).isEqualTo(upper);
+  }
+
+  @Test
+  public void testVariantsAreLowercased() {
+    Locale locale = new Locale("en", "US", "variant");
+
+    assertThat(locale.getVariant()).isEqualTo("variant");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-US-variant");
+  }
+
+  @Test
+  public void testInvalidShortVariantsAreConvertedToExtensions() {
+    Locale locale = new Locale("en", "US", "1");
+
+    // The toString value gives the variant value as is.  Since the toString value is not part of any specification,
+    // the variant part can be as many characters as you want.
+    assertThat(locale.toString()).isEqualTo("en_US_1");
+
+    // Variants must be between 5-8 characters, if they are too short, they are converted to extensions.
+    assertThat(locale.getVariant()).isEqualTo("1");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-US-x-lvariant-1");
+
+    // They can be converted back to Locales as well.
+    Locale converted = Locale.forLanguageTag("en-US-x-lvariant-1");
+
+    assertThat(converted.getVariant()).isEqualTo("1");
+
+    Locale extension = new Locale.Builder()
+        .setLanguage("en")
+        .setRegion("US")
+        .setExtension('x', "lvariant-1")
+        .build();
+    assertThat(extension).isEqualTo(converted);
+  }
+
+  @Test
+  public void testInvalidLongVariantsAreDiscarded() {
+    Locale locale = new Locale("en", "US", "thisvariantistoolong");
+
+    // The toString value gives the variant value as is.  Since the toString value is not part of any specification,
+    // the variant part can be as many characters as you want.
+    assertThat(locale.toString()).isEqualTo("en_US_thisvariantistoolong");
+
+    // Variants must be between 5-8 characters, if they are too long, they are dropped.  This is because extensions must
+    // be between 2-8 characters, and separated by hyphens.  Converting to an extension would result in a too long
+    // extension, and instead the variant is dropped when creating a language tag.
+    assertThat(locale.getVariant()).isEqualTo("thisvariantistoolong");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-US");
+  }
+
+  @Test
+  public void testInvalidExtensionsThatAreTooShort() {
+    Locale locale = new Locale.Builder()
+        .setLanguage("en")
+        .setExtension('x', "1")
+        .build();
+
+    assertThat(locale.toString()).isEqualTo("en__#x-1");
+    assertThat(locale.toLanguageTag()).isEqualTo("en-x-1");
+
+    locale = new Locale.Builder()
+        .setLanguage("en")
+        .setExtension('x', "")
+        .build();
+
+    assertThat(locale.toString()).isEqualTo("en");
+    assertThat(locale.toLanguageTag()).isEqualTo("en");
+  }
+
+  @Test
+  public void testInvalidExtensionsThatAreTooLongAreIllformed() {
+    Throwable thrown = catchThrowable(() -> new Locale.Builder()
+        .setLanguage("en")
+        .setExtension('x', "thisextensionistoolong")
+        .build());
+
+    assertThat(thrown).isExactlyInstanceOf(IllformedLocaleException.class);
+  }
+}
